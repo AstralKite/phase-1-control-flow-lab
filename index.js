a6ba31776464d52ef682d4590f68d74f09711594
Fausto Ramirez <ramirezmedia@gmail.com>
2022-11-22 21:43:28 -0500

    commit tree ef2341bb65b6cf4d269af4a194134f86df561676

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+  if(num < 400)
+    return "This one is on me!";
+
+  if(num > 400 && num <2000 )
+    return 'That will be twenty bucks.';
+    
+  if(num > 2500 )
+    return 'No can do.';
+
+  if(num > 2000 )
+    return 'I will gladly take your thirty bucks.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(dest){
+  return dest === 'NYC' ? 'Ok, sounds good.' : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+
+    case ('generous'):
+      return "Thank you so much.";
+      break;
+
+    case ('not as generous'):
+      return "Thank you.";
+      break;
+
+    default:
+      return "Bye.";
+  }
 }
